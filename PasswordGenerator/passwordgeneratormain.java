7b0dcc618bdc05dd12074ac5efe5bdeda688be82
shubham-sahay64 <146697900+shubham-sahay64@users.noreply.github.com>
2024-10-17 13:45:39 +0530

    Create passwordgeneratormain.java

---
+++ PasswordGenerator/passwordgeneratormain.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        PasswordGeneratorGui passwordGeneratorGui = new PasswordGeneratorGui();
+        passwordGeneratorGui.setVisible(true);
+    }
+}
